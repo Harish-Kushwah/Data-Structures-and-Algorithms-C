7736d8ee71b1c217eef3072ae6f7c0fb7500f5d8
Harish-Kushwah <harishkushwah54321@gmail.com>
2023-06-27 14:19:34 +0530

    functions added

--- src/linkedlist.h
+++ src/linkedlist.h
@@ -42,7 +42,140 @@ Node *createLinkedList()
  
     return head;
 }
+int length(Node *head)
+{
+    int len=0;
+    for(Node *temp=head;temp!=NULL; temp=temp->next) len++;
 
+    return len;
+
+}
+Node *insertAtBeginning(Node *head , int val)
+{
+    Node *new_node = createNode(val);
+    new_node->next = head;
+    head = new_node;
+    return head; 
+}
+
+Node *insertAtEnd(Node *head,int val)
+{
+    Node *new_node = createNode(val);
+    new_node->next =NULL;
+
+    Node *temp=NULL;
+    for(temp=head;temp->next!=NULL;temp = temp->next);
+
+    temp->next = new_node;
+
+    return head;
+    
+}
+
+Node *insertAtPosition(Node *head,int pos ,int val)
+{
+      int len = length(head);
+
+      if(pos<0 || pos >len){
+        printf("Enter valid postion");
+        return NULL;
+      }
+
+      if(pos==0){
+        return insertAtBeginning(head,val);
+      }
+      else if(pos == len){
+        return insertAtEnd(head,val);
+      }
+
+      Node *new_node = createNode(val);
+      int count=0;
+      Node *previous_node=head;
+      for(Node *temp = head;temp!=NULL; temp= temp->next){
+        if(pos == count){
+            previous_node->next = new_node;
+            new_node->next=temp;
+            break;
+        }
+        else{
+          previous_node = temp;
+        }
+
+        count++;
+      }
+      return head;
+}
+
+Node *deleteFromBeginning(Node *head)
+{
+    Node *temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+}
+Node *deleteFromEnd(Node *head)
+{
+    Node *temp = head;
+    for(temp=head; temp->next!=NULL;temp=temp->next)
+    {
+        if(temp->next->next==NULL)
+        {
+           
+            temp->next=NULL;
+            free(temp->next);
+            break;
+        }
+    }
+    return head;
+}
+
+Node *deleteAtPosition(Node *head,int pos)
+{
+      int len = length(head);
+
+      if(pos<=0 || pos >len){
+        printf("Enter valid postion");
+        return NULL;
+      }
+
+      if(pos==1){
+        return  deleteFromBeginning(head);
+      }
+      else if(pos == len){
+        return deleteFromEnd(head);
+      }
+
+      int count=1;
+      Node *previous_node=head;
+      for(Node *temp = head;temp!=NULL; temp= temp->next){
+        if(pos == count){
+            previous_node->next =temp->next;
+            break;
+        }
+        else{
+          previous_node = temp;
+        }
+        count++;
+      }
+      return head;
+}
+
+void search(Node *head , int key)
+{
+    int flag =0;
+    for(Node *temp = head; temp!=NULL; temp = temp->next)
+    {
+        if(temp->val==key)
+        {
+            printf("\n%d found in List\n",key);
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0){
+        printf("\n%d not found  list\n",key);
+    }
+}
 void printLinkedList(Node *head)
 {
     printf("Linked list data :");
@@ -50,4 +183,6 @@ void printLinkedList(Node *head)
     {
         printf("%d " , temp->val);
     }
+
+    printf("\n");
 }
--- src/main.c
+++ src/main.c
@@ -3,6 +3,59 @@
 
 int main()
 {
+    //create a linked list
     Node * head = createLinkedList();
+
+    //insert node at beginning
+    //new_node created with value 10 and added at the beginning
+    head = insertAtBeginning(head,10);
+
+    //prints after inserting node
+    printf("\nLinked list data after after inserting node at beginning \n");
     printLinkedList(head);
+
+    //insert node at End
+    //new_node created with value 7 and added at the end
+    head = insertAtEnd(head,7);
+
+    //prints after inserting node
+    printf("\nLinked list data after after inserting node at End \n");
+    printLinkedList(head);
+
+ 
+    //Insert node at any postion
+    printf("\nInsert at position\n ");
+    int position = 0;
+    int val = 12;
+    head = insertAtPosition(head,position,val);
+    printLinkedList(head);   
+
+    //delete node from the beginning
+    printf("\nAfter deleting node from the beginning \n");
+    head = deleteFromBeginning(head);
+    printLinkedList(head);
+
+    //delete node from the end
+    printf("\nAfter deleting node from the end \n");
+    head = deleteFromEnd(head);
+    printLinkedList(head);
+
+    //Delete at any postion
+    printf("\nDelete at position\n ");
+    position = 2;
+    head = deleteAtPosition(head,position);
+    printLinkedList(head); 
+
+     
+    //search element in the linked list
+    int key = 10;
+    search(head,key);
+
+    // prints the elements of linked list
+    printLinkedList(head);
+
+   //find the length of the Linked List 
+   int len = length(head);
+   printf("Total length of the Linked List : %d" ,len);
+
 }
